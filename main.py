c02ae4819b48247ef3262aa923e141f3fdbd2bbd
shmonder-glitch <55046615+shmonder-glitch@users.noreply.github.com>
2020-11-21 18:36:53 +0300

    release 1.1

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from random import randint
 
-from PyQt5 import uic
+from ui import Ui_MainWindow
 from PyQt5.QtCore import QPoint
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QMainWindow, QApplication
 import sys
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
 
     def run(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         rad = randint(0, 200)
         qp.drawEllipse(QPoint(randint(0, 800), randint(0, 500)), rad, rad)
 
